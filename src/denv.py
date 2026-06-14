82a4857a5f23aa038e148d46618312134d43781f
Alex Pesta <ayushchaudhury01@outlook.com>
2026-06-14 16:06:47 +0530

    Add denv.py script for environment setup

---
+++ src/denv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: denv.py <typefile> <run-setting : optional>")
+    exit()
+
+arg = sys.argv
+
+if arg[1] == "-c":
+    if len(arg) <= 2:
+        subprocess.run(["bash", "scripts/c/c-default.sh"])
+    else:
+        if len(arg) >= 3:
+            if arg[2] == "-C-All":
+                subprocess.run(["bash", "scripts/c/c-all.sh"])
+
+elif arg[1] == "-cpp" or arg[1] == "-c++":
+    if len(arg) <= 2:
+        subprocess.run(["bash", "scripts/cpp/cpp-default.sh"])
+    else:
+        if len(arg) >= 3:
+            if arg[2] == "-CPP-All":
+                subprocess.run(["bash", "scripts/cpp/cpp-all.sh"])
+
+elif arg[1] == "-py" or arg[1] == "-python":
+    if len(arg) <= 2:
+        subprocess.run(["bash", "scripts/python/python-default.sh"])
+    else:
+        if len(arg) >= 3:
+            if arg[2] == "-no-pycache":
+                subprocess.run(["bash", "scripts/python/python-no-pycache.sh"])
